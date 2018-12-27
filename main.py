cbf0e61de40e5c947fffb9b399c3d4a37fe48949
Taylor Page <tpage938@gmail.com>
2018-12-27 23:58:08 +0000

    coded player movement

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ player = pygame.image.load("racecar.png")
 gameOver = False
 x = (width * 0.45)
 y = (height * 0.8)
+x_change = 0
 
 def displayplayer(x,y):
     window.blit(player, (x,y))
@@ -25,7 +26,16 @@ while not gameOver:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             gameOver = True
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a:
+                x_change = -5
+            if event.key == pygame.K_d:
+                x_change = 5
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_a or event.key == pygame.K_d:
+                x_change = 0
         print(event)
+    x += x_change
     window.fill(white)
     displayplayer(x,y)
 
